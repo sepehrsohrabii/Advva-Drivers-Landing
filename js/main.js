43b56bcfedebd1e8c40a352d26adbb4dac262e08
Sepehr Sohrabi <sepehr0sohrabi@gmail.com>
2022-04-01 18:04:25 +0430

    Adding motion on scrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,343 @@
 $(document).ready(function() {
-  
+/* LazyLoad - START */
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.from(".section-1", {
+    scrollTrigger: {
+      trigger: ".section-1",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -100,
+    opacity: 0,
+    duration: 0.5,
+  });
+  gsap.from(".section-2", {
+    scrollTrigger: {
+      trigger: ".section-2",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-3", {
+    scrollTrigger: {
+      trigger: ".section-3",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-4", {
+    scrollTrigger: {
+      trigger: ".section-4",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-5", {
+    scrollTrigger: {
+      trigger: ".section-5",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-6", {
+    scrollTrigger: {
+      trigger: ".section-6",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-7", {
+    scrollTrigger: {
+      trigger: ".section-7",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-8", {
+    scrollTrigger: {
+      trigger: ".section-8",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-9", {
+    scrollTrigger: {
+      trigger: ".section-9",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-10", {
+    scrollTrigger: {
+      trigger: ".section-10",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-11", {
+    scrollTrigger: {
+      trigger: ".section-11",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-12", {
+    scrollTrigger: {
+      trigger: ".section-12",
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 2
+  });
+  gsap.from(".section-13", {
+    scrollTrigger: {
+      trigger: ".section-13",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -100,
+    
+    duration: 2
+  });
+  gsap.from(".section-14", {
+    scrollTrigger: {
+      trigger: ".section-14",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -100,
+    duration: 2
+  });
+  gsap.from(".section-15", {
+    scrollTrigger: {
+      trigger: ".section-15",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -100,
+    duration: 2
+  });
+  gsap.from(".section-16", {
+    scrollTrigger: {
+      trigger: ".section-16",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-17", {
+    scrollTrigger: {
+      trigger: ".section-17",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-18", {
+    scrollTrigger: {
+      trigger: ".section-18",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-19", {
+    scrollTrigger: {
+      trigger: ".section-19",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    scale: 0.5,
+    y: -100,
+    duration: 2
+  });
+  gsap.from(".section-20", {
+    scrollTrigger: {
+      trigger: ".section-20",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -100,
+    duration: 2
+  });
+  gsap.from(".section-21", {
+    scrollTrigger: {
+      trigger: ".section-21",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    scale: 0.8,
+    duration: 2
+  });
+  gsap.from(".section-22", {
+    scrollTrigger: {
+      trigger: ".section-22",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    scale: 0.8,
+    duration: 2
+  });
+  gsap.from(".section-23", {
+    scrollTrigger: {
+      trigger: ".section-23",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    scale: 0.8,
+    duration: 2
+  });
+  gsap.from(".section-24", {
+    scrollTrigger: {
+      trigger: ".section-24",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    scale: 0.8,
+    duration: 2
+  });
+  gsap.from(".section-25", {
+    scrollTrigger: {
+      trigger: ".section-25",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -100,
+    duration: 2
+  });
+  gsap.from(".section-26", {
+    scrollTrigger: {
+      trigger: ".section-26",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    x: -50,
+    duration: 2
+  });
+  gsap.from(".section-27", {
+    scrollTrigger: {
+      trigger: ".section-27",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    x: 400,
+    duration: 2
+  });
+  gsap.from(".section-28", {
+    scrollTrigger: {
+      trigger: ".section-28",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-29", {
+    scrollTrigger: {
+      trigger: ".section-29",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-30", {
+    scrollTrigger: {
+      trigger: ".section-30",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    x: -50,
+    duration: 2
+  });
+  gsap.from(".section-31", {
+    scrollTrigger: {
+      trigger: ".section-31",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-32", {
+    scrollTrigger: {
+      trigger: ".section-32",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: +50,
+    duration: 2
+  });
+  gsap.from(".section-33", {
+    scrollTrigger: {
+      trigger: ".section-33",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    x: -50,
+    duration: 2
+  });
+  gsap.from(".section-34", {
+    scrollTrigger: {
+      trigger: ".section-34",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    x: +50,
+    duration: 2
+  });
+  gsap.from(".section-35", {
+    scrollTrigger: {
+      trigger: ".section-35",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-36", {
+    scrollTrigger: {
+      trigger: ".section-36",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  gsap.from(".section-37", {
+    scrollTrigger: {
+      trigger: ".section-37",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    y: -50,
+    duration: 2
+  });
+  /*
+  gsap.from(".section-38", {
+    scrollTrigger: {
+      trigger: ".section-38",
+      toggleActions: "play reverse play reverse",
+      scrub: 1,
+    },
+    scale: 0.5,
+    duration: 2
+  }); */
+/* LazyLoad - END */
+
+
+
 });
